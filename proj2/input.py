180a7089e207642a332bfaef1982d7a96a87838a
Srikar Godilla <srikar.godilla@yale.edu>
2022-09-29 14:59:24 -0400

    AUTO: Raspi IP address [Thu 29 Sep 2022 02:59:24 PM EDT]

--- proj2/input.py
+++ proj2/input.py
@@ -3,11 +3,13 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
+GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(23, GPIO.IN)
 GPIO.setup(24, GPIO.IN)
+GPIO.setup(25, GPIO.IN)
 
 start_time = time.time()
-seconds = 30
+seconds = 60
 interval = 0.05
 buffer = 0
 
@@ -16,9 +18,11 @@ while True:
     elapsed_time = current_time - start_time
     X = GPIO.input(23)
     Y = GPIO.input(24)
+    Button = GPIO.input(22)
+    Switch = GPIO.input(25)
 
-    if ((elapsed_time - buffer > interval) and (X < 0.5 or Y < 0.5)):
-        print("X: ", X, ", Y: ", Y, ", Time: ", elapsed_time)
+    if ((elapsed_time - buffer > interval)):
+        print("X: ", X, ", Y: ", Y, ", Button: ", Button, ", Switch: ", Switch, ", Time: ", elapsed_time)
         buffer = elapsed_time
 
     if elapsed_time > seconds:
