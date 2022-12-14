42b8f9e1e4d7b4020887cc0be4d484154a4dcda3
Srikar Godilla <srikar.godilla@yale.edu>
2022-12-14 16:18:31 -0500

    fixed variable frequency

--- Project6/read_soft_pot.py
+++ Project6/read_soft_pot.py
@@ -66,5 +66,3 @@ while True:
         # ADC (ADS1015 = 12-bit, ADS1115 = 16-bit).
     # Print the ADC values.
     #print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*values))
-    # Pause for half a second.
-    time.sleep(0.5)
